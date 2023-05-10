c874981ec917ba7f64a286753037a33578b4adea
Sebastian Gilits <sep.gil@gmail.com>
2023-05-10 10:09:04 +0200

    Allow to select input tile

--- src/component/tiles/InputRow.tsx
+++ src/component/tiles/InputRow.tsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { useState } from "react";
 import { InputTile } from "./InputTile";
 
 interface Props {
   wordLen: number;
+  defaultActiveTile?: number;
 }
 
-export const InputRow = ({ wordLen }: Props) => {
+export const InputRow = ({ wordLen, defaultActiveTile = undefined }: Props) => {
+  const [activeTile, setActiveTile] = useState<number | undefined>(
+    defaultActiveTile
+  );
+
   const inputTiles = Array(wordLen)
     .fill(null)
     .map((v, i) => {
-      return <InputTile key={i} />;
+      return (
+        <InputTile
+          onClick={() => {
+            setActiveTile(i);
+          }}
+          key={i}
+          active={activeTile == i}
+        />
+      );
     });
 
   return <>{inputTiles}</>;
--- src/component/tiles/InputTile.tsx
+++ src/component/tiles/InputTile.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 interface Props {
   letter?: string;
+  onClick?(): void;
+  active?: boolean;
 }
 
-export const InputTile = ({ letter = " " }: Props) => (
-  <div className="border border-gray-500 inline-flex items-center justify-center bg-white text-lg font-semibold rounded h-[2rem] w-[2rem]">
-    {letter}
-  </div>
-);
+export const InputTile = ({
+  letter = " ",
+  onClick: onSelect = () => {},
+  active = false,
+}: Props) => {
+  let styling =
+    "border inline-flex items-center justify-center bg-white text-lg font-semibold rounded h-[2rem] w-[2rem] cursor-pointer";
+
+  if (active) {
+    styling += " border-blue-500";
+  } else {
+    styling += " border-gray-500";
+  }
+
+  return (
+    <div onClick={onSelect} className={styling}>
+      {letter}
+    </div>
+  );
+};
