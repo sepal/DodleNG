f4c09cb47f5541ef05d66de1280316e08eaa53ae
Sebastian Gilits <sep.gil@gmail.com>
2023-05-11 16:49:52 +0200

    Add test active input tile in inputrow.

---
+++ src/component/tiles/__tests__/inputRow.test.tsx
@@ -0,0 +1,33 @@
+/**
+ * @author codelity_proto@0.0.1
+ * @request Create a test for InputRow that makes sure you can switch between
+ *   several InputTiles.
+ */
+import { render, fireEvent } from "@testing-library/react";
+import { GameProvider } from "../../../lib/gameContext";
+import { InputRow } from "../InputRow";
+
+describe("InputRow", () => {
+  test("can switch between several InputTiles", () => {
+    const word = "test";
+    const { getAllByTestId } = render(
+      <GameProvider word={word}>
+        <InputRow />
+      </GameProvider>
+    );
+
+    const inputTiles = getAllByTestId("input-tile");
+
+    fireEvent.click(inputTiles[0]);
+    fireEvent.keyDown(document, { key: "a" });
+    expect(inputTiles[0].textContent).toBe("a");
+
+    fireEvent.click(inputTiles[1]);
+    fireEvent.keyDown(document, { key: "b" });
+    expect(inputTiles[1].textContent).toBe("b");
+
+    fireEvent.click(inputTiles[0]);
+    fireEvent.keyDown(document, { key: "c" });
+    expect(inputTiles[0].textContent).toBe("c");
+  });
+});
